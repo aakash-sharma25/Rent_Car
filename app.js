366ced6c34f98227d014a37785c5f9e28689d871
aakash-sharma25 <as024harma@gmail.com>
2024-04-14 12:15:39 +0530

    final code for deployment

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const express= require("express");
+const express = require("express");
 const cors = require("cors");
 const { connectDB } = require("./config/connectDB");
-const userRoute = require("./routes/userRoute")
-const carRoute = require("./routes/carRoute")
+const userRoute = require("./routes/userRoute");
+const carRoute = require("./routes/carRoute");
+const path = require("path");
 
 require("dotenv").config();
 
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 //database connection
-connectDB(); 
+connectDB();
 //routes
 
-app.use("/api/v1/auth" , userRoute)
-app.use("/api/v1/car" , carRoute)
+app.use("/api/v1/auth", userRoute);
+app.use("/api/v1/car", carRoute);
 
+if (process.env.MODE === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
